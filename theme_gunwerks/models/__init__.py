7441d0c85dc3310e6d01745f6520ce1344b52c7c
Andrew Hastings <32532163+andrewdhastings@users.noreply.github.com>
2019-10-03 16:49:30 -0600

    just ballistics

--- theme_gunwerks/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import settings
-from . import megamenu
-from . import gunwerks_configuration
-from . import event
-from . import res_partner
-from . import product
-from . import blog
--- web_html_snippet/static/src/js/embeded.js
+++
@@ -1,7 +0,0 @@
-odoo.define('web_html_snippet.embeded_js', function(require){
-$(document).ready(function(){
-	$(".o_mail_theme_selector button[data-event='fullscreen']").click(function(ev){
-	console.log("Sdsdsdsd");
-	})
-});
-});
